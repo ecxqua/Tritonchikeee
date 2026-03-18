f9f710109065757090e8c1b9c8d62503e88dc95e
Timur Yunusov <140435421+flitme@users.noreply.github.com>
2026-03-18 14:12:20 +0500

    new_w (#2)
    * new_w
    
    * loading script
    
    * separate download script
    
    * separate download script fix
    
    ---------
    
    Co-authored-by: Yulia Davletshina <113159353+ecxqua@users.noreply.github.com>

--- bot/app.py
+++ bot/app.py
@@ -6,6 +6,7 @@ from aiogram.client.default import DefaultBotProperties
 from bot.handlers.user_private import user_private_router
 from dotenv import find_dotenv, load_dotenv
 import yaml
+import gdown
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
 ENV_PATH = ROOT_DIR / '.env'
@@ -28,6 +29,38 @@ def load_config(config_path="config/config.yaml"):
     except FileNotFoundError:
         raise FileNotFoundError("Добавьте файл конфигурации для анализа: config/config.yaml")
 
+# Функция загрузки папки весов из Google Drive
+def download_models_folder():
+    models_dir = Path("models")
+    required_files = [
+        models_dir / "best_seg.pt",
+        models_dir / "best_model.pth",
+    ]
+    folder_url = (
+        "https://drive.google.com/drive/folders/"
+        "1OjB1VAS6FyROYeWpnmkwlt80AOPEq9_7?usp=drive_link"
+    )
+
+    models_dir.mkdir(exist_ok=True)
+
+    if all(path.exists() for path in required_files):
+        print("Файлы весов уже есть")
+        return
+
+    print(f"Загружаю файлы в {models_dir.resolve()}")
+
+    try:
+        gdown.download_folder(
+            url=folder_url,
+            output=str(models_dir),
+            quiet=False,
+            use_cookies=False,
+            remaining_ok=True,
+        )
+        print("Загрузка весов завершена")
+    except Exception as e:
+        print(f"Ошибка при загрузке весов: {e}")
+        raise
 
 bot = Bot(token=token, default=DefaultBotProperties())
 bot.config = load_config()
@@ -48,7 +81,7 @@ async def on_shutdown():
 async def main():
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
-
+    download_models_folder()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
