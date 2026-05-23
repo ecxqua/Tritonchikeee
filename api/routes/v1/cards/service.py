2dd3deccda914da8b6d9f8c5ceeb1d263c53f6e9
discocat <me@runderscore.com>
2026-05-23 15:03:11 +0500

    Incremental changes (14/?)

--- api/routes/v1/cards/service.py
+++ api/routes/v1/cards/service.py
@@ -1,3 +1,4 @@
+from api.error import APIError
 from services.card_service import ALLOWED_FIELDS
 from services.identification_service import IdentificationService
 
@@ -8,7 +9,13 @@ def get_card_history(
     card_id: str,
     id_service: IdentificationService
 ) -> Dict[str, Any]:
-    template_type = id_service.card_service.get_card(card_id)["template_type"]
+    print(f"Getting history of: {card_id}")
+    print(f"Card: {id_service.card_service.get_card(card_id)}")
+    card = id_service.card_service.get_card(card_id)
+    if not card:
+        raise APIError(status=400, msg=f"No card by ID {card_id}")
+
+    template_type = card["template_type"]
     template_type = template_type if len(template_type) == 4 else (
         template_type[0:2] + '-' + template_type[-1]
     )
